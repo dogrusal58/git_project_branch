8a7b2cfd63ddf462e0358111031f4d9285c069c2
dogrusal58 <dogrusalhoca58@gmail.com>
2020-08-16 19:38:13 +0300

    19:37 feature branch

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -4,6 +4,8 @@ public class BranchDeneme {
 
 	public static void main(String[] args) {
 		System.out.println("19:28 - master branchten ilk commit");
+		System.out.println("19:36 - feature branchten ilk komut");
+		
 		
 
 	}
